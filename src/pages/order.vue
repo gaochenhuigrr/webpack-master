c5395fcb335c4b1017ddb0018f1a8416c2ff08c9
gaoch1 <chenhui.gao@meicloud.com>
2020-03-23 13:36:01 +0800

    引入axios

--- src/pages/order.vue
+++ src/pages/order.vue
@@ -2,11 +2,16 @@
   <div>
     <h1>订单页面</h1>
     <el-button type="success" plain @click="increment">{{count}}</el-button>
+    <h3>Articles</h3>
+    <ul v-for="({node}, i) in articles" :key="i">
+      <li @click="jump(node.originalUrl)">{{node.title}}</li>
+    </ul>
   </div>
 </template>
 
 <script>
 import { mapState, mapActions } from 'vuex'
+import axios from 'axios'
 export default {
   name: 'order',
   components: {},
@@ -17,7 +22,7 @@ export default {
   },
   data() {
     return {
-
+      articles: []
     }
   },
   computed: {
@@ -28,10 +33,35 @@ export default {
 
   },
   mounted () {
-
+    this.getHotArticles()
   },
   methods: {
-    ...mapActions(['increment'])
+    ...mapActions(['increment']),
+    getHotArticles () {
+      axios.post('https://web-api.juejin.im/query', {
+        operationName: '',
+        query: '',
+        variables:{
+          first: 20,
+          after: '',
+          order: 'THREE_DAYS_HOTTEST'
+        },
+        extensions:{
+          query:{
+              id: '21207e9ddb1de777adeaca7a2fb38030'
+          }
+        }
+      }, {
+        headers: {'X-Agent': 'Juejin/Web'},
+        'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, PATCH',
+        'Access-Control-Allow-Origin': '*'
+      }).then(res => {
+        this.articles = res.data.data.articleFeed.items.edges
+      })
+    },
+    jump (url) {
+      window.open(url, '_blank')
+    }
   }
 }
 </script>
