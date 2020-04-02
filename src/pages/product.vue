403561710ba32a1685aa1ece4e5d172af87e7bae
gaoch1 <chenhui.gao@meicloud.com>
2020-04-02 10:13:00 +0800

    引入mockjs

--- src/pages/product.vue
+++ src/pages/product.vue
@@ -1,10 +1,13 @@
 <template>
   <div>
     <h1>商品页面</h1>
+    <h1>All Star</h1>
+    <p v-for="(item, idx) in candidateSet" :key="idx">{{item.star}}</p>
   </div>
 </template>
 
 <script>
+const Mock = require('mockjs')
 export default {
   name: 'product',
   components: {},
@@ -15,7 +18,7 @@ export default {
   },
   data() {
     return {
-
+      candidateSet: []
     }
   },
   computed: {},
@@ -24,10 +27,27 @@ export default {
 
   },
   mounted () {
-
+    this.getMockData()
   },
   methods: {
-
+    getMockData () {
+      const mockData = Mock.mock({
+        "data|1-5": [
+          {
+            "star|1": [
+              "Curry",
+              "James",
+              "Davis",
+              "Harden",
+              "KD",
+              "Irving"
+            ]
+          }
+        ]
+      })
+      this.candidateSet = mockData.data
+      return mockData
+    }
   }
 }
 </script>
